ef7fd41261934128a1c4e9b2702f5fdfcdb2f72b
Grace Flores <grace-flores@uiowa.edu>
2023-07-07 11:43:18 -0500

    used Sebastian's method of scaling, added a heaviside 1 boxcar example

--- communication_simulation.m
+++ communication_simulation.m
@@ -21,7 +21,7 @@ pulse_width = 0.5;          % width of each rectangular pulse
 pulse_interval = 1;         % interval between pulse
 
 
-% Example: 1 boxcar 
+% Example: 1 boxcar (rectpuls) 
 % for n = 0:pulse_interval:0    
 %     x_t = x_t + rectpuls(t-n-(pulse_width/2), pulse_width);
 % end
@@ -39,7 +39,50 @@ for n = 0:pulse_interval:total_duration
 end
 
 
+% Example: 1 boxcar (heaviside)
+% center = 1;
+% x_t = heaviside(t - center + pulse_width/2) - heaviside(t - center - pulse_width/2);
+
+
 %--------------------------------------------------------------------------
+
+
+% % Sebastian's code
+% 
+% t = -20:0.01:20;    %Adjusted increment to 0.01
+% u = heaviside(t+5.5);
+% 
+% % Making an array of shifts
+% countA = 1;
+% uNum = [];
+% iNum = 5.5;
+% while (countA < 2)
+%     if (mod(countA,2) == 1)
+%         iNum = iNum - 0.5;
+%     else
+%         iNum = iNum - 1;
+%     end
+%     uNum(countA) = iNum;
+%     countA = countA + 1;
+% end
+% 
+% % 10 Cycles of unit step in one variable
+% count = 0;
+% for T = uNum
+%     if (mod(count,2) == 0)
+%         u = u - heaviside(t+T);
+%     else
+%         u = u + heaviside(t+T);
+%     end
+%     count = count + 1;
+% end
+% 
+% 
+% x_t = u;
+
+
+%--------------------------------------------------------------------------
+
 
 
 % Define the channel impulse response
@@ -61,11 +104,9 @@ h_t = exp(-0.2*t); % e^-0.2 , e^-0.4 , ...
 
 
 % Convolve the input with the channel impulse response
-y_t = conv(x_t, h_t);
+y_t = 0.01*conv(x_t, h_t);          % 0.01 to appropriately scale y-axis 
 
-y_t = y_t*0.01;                         % appropriately scale y-axis*
-t_output = 0:ts:total_duration*2-ts-ts; % create new time axis for output
-
+y_time = 0.01*(1:length(y_t));      % new time vector  
 
 
 % *Note: y_t output is based on original x_t and h_t x-axis units
@@ -83,9 +124,9 @@ h_t_inv = ifft(h_f_inv);                    % back to time domain
 
 
 % Convolve output with inverse impulse response
-z_t = conv(y_t,h_t_inv);    % equalized signal  
+z_t = 0.01*conv(y_t,h_t_inv);               % equalized signal 
 
-z_t = z_t(1:1000);          % cut to size  
+z_time = 0.1*(1:length(z_t));               % new time vector 
 
 
 %--------------------------------------------------------------------------
@@ -110,21 +151,21 @@ title('Channel Impulse Response');
 
 % y_t
 subplot(5,1,3);
-plot(t_output,y_t);
+plot(y_time,y_t);
 xlabel('Time');
 ylabel('Amplitude');
 title('Output Signal');
 
-% h_t_inv
-subplot(5,1,4);
-plot(t,h_t_inv);
-xlabel('Time');
-ylabel('Amplitude');
-title('Channel Inverse Impulse Response');
+% % h_t_inv
+% subplot(5,1,4);
+% plot(t,h_t_inv);
+% xlabel('Time');
+% ylabel('Amplitude');
+% title('Channel Inverse Impulse Response');
 
 % z_t (equalized signal)
 subplot(5,1,5);
-plot(t,z_t);
+plot(z_time,z_t);
 xlabel('Time');
 ylabel('Amplitude');
 title('Equalized Signal');
