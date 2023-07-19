c44aac6fa50b492350efcffceabacf192a8c908e
Sebastian Parecattil <sebastian-parecattil@uiowa.edu>
2023-07-19 14:30:46 -0500

    Before meeting 7/19

--- Copy_of_linear_equalizer.m
+++ Copy_of_linear_equalizer.m
@@ -5,7 +5,7 @@ clc
 
 % Parameters 
 ts = 0.01;
-numSymbols = 1000;
+numSymbols = 5000;
 numTrainingSymbols = 200;
 
 
@@ -17,14 +17,26 @@ M = 2;  % BPSK
 data = randi([0 1],numSymbols,1);
 x = pskmod(data,M);
 
+%-------------------------------------------------------------------------%
+
 % Simple Examples 
 % h = 1;
 % h = [0.02+0.5i 0.05];
 
-% Ideal Example
-t_h = (0:ts:length(x)*ts)';
-h = exp(-0.2*t_h);
-h = h(1:2);
+% % Ideal Example
+% t_h = (0:ts:length(x)*ts)';
+% h = exp(-0.2*t_h);
+% h = h(1:2);
+
+% Decreasing Exponential Example
+t_h = (0:1:numSymbols)';
+h = 0.5.^t_h;
+
+% % Simple Example
+% h = [1 0.5 0.25];
+
+% % Complex Example
+% h = cos(0:1:100);
 
 y = conv(x,h);
 % rxSig = awgn(rxSig,30)
@@ -35,21 +47,17 @@ y = conv(x,h);
 % Equalization
 % research more 
 
-% lineq = comm.LinearEqualizer( ...
-%     NumTaps=8, ...
-%     StepSize=0.1, ...
-%     Constellation=complex([-1 1]), ...
-%     ReferenceTap=4);
-% 
-% [z,err] = lineq(y,x(1:numTrainingSymbols)); 
+lineq = comm.LinearEqualizer( ...
+    NumTaps=8, ...
+    StepSize=0.1, ...
+    Constellation=complex([-1 1]), ...
+    ReferenceTap=4);
 
-dfeq = comm.DecisionFeedbackEqualizer( ...
-    'Algorithm','LMS', ...
-    'NumForwardTaps',4, ...
-    'NumFeedbackTaps',3, ...
-    'StepSize',0.01);
+[z,err] = lineq(y,x(1:numTrainingSymbols)); 
+%[z,err] = lineq(y,x); 
 
-z = dfeq(y,x);
+% dfeq = comm.DecisionFeedbackEqualizer('Algorithm','LMS','NumForwardTaps',4,'NumFeedbackTaps',3,'StepSize',0.01,'Constellation',complex([-1 1]));
+% z = dfeq(y,x);
 
 %-------------------------------------------------------------------------%
 
@@ -62,7 +70,9 @@ constdiag = comm.ConstellationDiagram( ...
     ChannelNames={'Before equalization','After equalization'}, ...
     ReferenceConstellation=pskmod([0 M-1],M));
 
-constdiag(y(numSymbols/2:end),z(numSymbols/2:end));
+%constdiag(y(numSymbols/2:end),z(numSymbols/2:end));
+% constdiag(y(numTrainingSymbols:(numSymbols/2)),z(numTrainingSymbols:(numSymbols/2)));
+constdiag(y,z);
 
 %-------------------------------------------------------------------------%
 
